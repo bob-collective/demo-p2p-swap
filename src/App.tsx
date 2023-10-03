9e3a096dffe36637e4506ab8bd77a85d11db271b
Peter <peter@interlay.io>
2023-10-03 14:46:28 +0200

    fix: prompt wallet connection in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { P2P } from './pages/P2P';
 import { V0 } from './pages/V0';
 import { Layout } from './components';
+import { useConnect } from 'wagmi';
+import { config } from './connectors/wagmi-connectors';
+import { useEffect } from 'react';
 
 function App() {
+  const { connect } = useConnect({ connector: config.connectors[0] });
+   
+  /** PROMPT WALLET CONNECTION */
+   useEffect(() => {
+    connect();
+  }, [connect]);
+
   return (
     <Layout>
       <BrowserRouter>
