8279e0eb7ed022de2b8f4d3f0313e47c91912973
Peter <peter@interlay.io>
2023-10-12 16:58:30 +0200

    feat: add sdk and provide real tx proof, connected to btc testnet

---
+++ src/utils/bitcoin.ts
@@ -0,0 +1,31 @@
+import { REGTEST_ESPLORA_BASE_PATH, MAINNET_ESPLORA_BASE_PATH, TESTNET_ESPLORA_BASE_PATH } from '@gobob/bob-sdk';
+
+const BITCOIN_NETWORK = import.meta.env.VITE_BITCOIN_NETWORK as string;
+
+const getElectrsUrl = () => {
+  if (BITCOIN_NETWORK === 'testnet') {
+    return TESTNET_ESPLORA_BASE_PATH;
+  }
+  if (BITCOIN_NETWORK === 'regtest') {
+    return REGTEST_ESPLORA_BASE_PATH;
+  }
+  if (BITCOIN_NETWORK === 'mainnet') {
+    return MAINNET_ESPLORA_BASE_PATH;
+  }
+  throw new Error(
+    `Invalid bitcoin network configured: ${BITCOIN_NETWORK}. Valid values are: testnet | regtest | mainnet.`
+  );
+};
+
+const fetchBtcNetwork = async (path: `/${string}`) => {
+  const electrsUrl = getElectrsUrl();
+  try {
+    const result = await fetch(`${electrsUrl}${path}`);
+    return await result.json();
+  } catch (err) {
+    console.error(`Error fetching regtest electrs server: ${err}`);
+    console.error('Make sure that electrs server is running at:', electrsUrl);
+  }
+};
+
+export { fetchBtcNetwork, getElectrsUrl, BITCOIN_NETWORK };
