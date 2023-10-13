0dfd5d1e2d922b4d874dac0655d822a276691688
Tom Jeatt <hello@tomjeatt.com>
2023-10-13 14:02:51 +0100

    add loading state and error handling

--- src/components/Faucet/Faucet.tsx
+++ src/components/Faucet/Faucet.tsx
@@ -2,18 +2,28 @@ import { CTA } from '@interlay/ui';
 import { usePublicClient } from 'wagmi';
 import { useContract } from '../../hooks/useContract';
 import { ContractType } from '../../constants';
+import { useState } from 'react';
 
 const Faucet = () => {
+  const [isLoading, setLoading] = useState(false);
+
   const { write } = useContract(ContractType.FAUCET);
   const publicClient = usePublicClient();
 
   const handleCallFaucet = async () => {
-    const tx = await write.mint();
-    await publicClient.waitForTransactionReceipt({ hash: tx });
+    setLoading(true);
+    try {
+      const tx = await write.mint();
+      await publicClient.waitForTransactionReceipt({ hash: tx });
+    } catch (e) {
+      window.alert(`Minting failed: ${e}`);
+      setLoading(false);
+    }
+    setLoading(false);
   };
 
   return (
-    <CTA onPress={() => handleCallFaucet()} size='small'>
+    <CTA loading={isLoading} onPress={() => handleCallFaucet()} size='small'>
       Get tokens
     </CTA>
   );
