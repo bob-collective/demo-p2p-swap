5a1376035d4a26f013a444680aa78cbeed5488e5
Tom Jeatt <hello@tomjeatt.com>
2023-10-20 11:16:55 +0100

    wip: fetch first ordinal to use as avatar

--- src/pages/AddressProofing/AddressProofing.tsx
+++ src/pages/AddressProofing/AddressProofing.tsx
@@ -10,6 +10,7 @@ const AddressProofing = (): JSX.Element => {
     isOpen: false
   });
   const [claimedAddress, setClaimedAddress] = useState<string>();
+  const [avatar, setAvatar] = useState<string>();
 
   const { address } = useAccount();
   const { read: readOwnership } = useContract(ContractType.OWNERSHIP);
@@ -26,20 +27,44 @@ const AddressProofing = (): JSX.Element => {
   // }
 
   useEffect(() => {
-    const getMessages = async () => {
-      if (!address) return;
+    // const getMessages = async () => {
+    //   if (!address) return;
 
-      const end = await readPing.id();
+    //   const end = await readPing.id();
 
-      for (let i = 0; i < end; i++) {
-        const poster = await readPing.posters([BigInt(i)]);
-        console.log('posters', i, poster);
-        console.log('posters', i, await readOwnership.ownedAddress([poster]));
-        console.log('messages', i, await readPing.messages([BigInt(i)])); // might need to convert i to bigint
-      }
+    //   for (let i = 0; i < end; i++) {
+    //     const poster = await readPing.posters([BigInt(i)]);
+    //     console.log('posters', i, poster);
+    //     console.log('posters', i, await readOwnership.ownedAddress([poster]));
+    //     console.log('messages', i, await readPing.messages([BigInt(i)])); // might need to convert i to bigint
+    //   }
+    // };
+
+    const fetchOrdinal = async () => {
+      if (!claimedAddress) return;
+
+      await fetch(`https://api.hiro.so/ordinals/v1/inscriptions?address=${claimedAddress}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setAvatar(data.results[0].id);
+        });
+
+      // const info = await fetch(
+      //   `https://open-api.unisat.io/v1/indexer/address/bc1pjak5p4sj7vrkh50ffz0ky3r270j60c29l0eurl98rh3ll7f3ewaqk27zmk/history`,
+      //   {
+      //     headers: {
+      //       Authorization: 'Bearer c986d2965dcc9cd0a18603697df266bbf7dfcf6ffd44d12beccecac22811dbfb'
+      //     }
+      //   }
+      // );
     };
 
-    getMessages();
+    fetchOrdinal();
   }, [address, claimedAddress, readOwnership, readPing]);
 
   useEffect(() => {
@@ -67,6 +92,7 @@ const AddressProofing = (): JSX.Element => {
           </CTA>
         )}
       </Flex>
+      {avatar && <img width='50' height='50' src={`https://static.unisat.io/content/${avatar}`} />}
       <ClaimAddressModal isOpen={isAddNewOrderModal.isOpen} onClose={handleCloseNewOrderModal} />
     </Flex>
   );
