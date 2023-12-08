ab1e9b8962fbfd13055ea80b0dabf686a6ee3ed8
Tom Jeatt <hello@tomjeatt.com>
2023-12-08 11:20:14 +0000

    Update TODO

--- src/pages/P2P/P2P.tsx
+++ src/pages/P2P/P2P.tsx
@@ -34,7 +34,7 @@ const P2P = (): JSX.Element => {
   return (
     <>
       <Flex flex={1} direction='column' gap='spacing6' justifyContent='center'>
-        {/* TODO: Replace info banner with accordion */}
+        {/* TODO: Replace info banner with FAQ accordion */}
         <Alert status='warning'>
           To fund your account you will need to get Sepolia ETH from a{' '}
           <a href={FAUCET_URL} rel='external' target='_blank'>
