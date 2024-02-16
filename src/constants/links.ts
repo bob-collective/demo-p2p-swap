2d35400239b088c08bc9d5dc1f875797850eed3f
Brendon Votteler <bvotteler@users.noreply.github.com>
2024-02-16 17:08:16 +1100

    change link to gelato bridge

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,6 @@
 const DOCS_URL = 'https://docs.gobob.xyz/docs/build/examples/btc-swap/';
 const GITHUB_URL = 'https://github.com/bob-collective';
 const FAUCET_URL = 'https://faucetlink.to/sepolia';
-const BRIDGE_URL = 'https://bridge.gobob.xyz//';
+const BRIDGE_URL = 'https://bridge.gelato.network/bridge/op-celestia-testnet';
 
 export { DOCS_URL, GITHUB_URL, FAUCET_URL, BRIDGE_URL };
