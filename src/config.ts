14939459a6afe6bfb65eb8fd13b9169fa19c3e21
Brendon Votteler <bvotteler@users.noreply.github.com>
2024-02-16 16:53:17 +1100

    add rpc, wss, explorer and contract addresses

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-const L2_RPC_URL = 'https://l2-puff-bob-jznbxtoq7h.t.conduit.xyz';
-const L2_WSS_URL = 'wss://l2-puff-bob-jznbxtoq7h.t.conduit.xyz';
-const L2_BLOCK_EXPLORER = 'https://explorerl2-puff-bob-jznbxtoq7h.t.conduit.xyz';
-const L2_CHAIN_ID = 111;
+const L2_RPC_URL = 'https://rpc.op-celestia-testnet.gelato.digital';
+const L2_WSS_URL = 'wss://ws.op-celestia-testnet.gelato.digital';
+const L2_BLOCK_EXPLORER = 'https://blockscout.op-celestia-testnet.gelato.digital';
+const L2_CHAIN_ID = 123420111;
 const L2_MULTICALL3_ADDRESS = '0x089b191d95417817389c8eD9075b51a38ca46DE8';
 
 export { L2_BLOCK_EXPLORER, L2_RPC_URL, L2_WSS_URL, L2_CHAIN_ID, L2_MULTICALL3_ADDRESS };
