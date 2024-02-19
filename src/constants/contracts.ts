5a375050b928f551bf44cddb1325f3e0f03a2fcd
Brendon Votteler <bvotteler@users.noreply.github.com>
2024-02-19 20:05:47 +1100

    add (incorrect) faucet back in to avoid breaking page

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,7 +1,7 @@
 import { BtcMarketplaceAbi } from '../contracts/abi/BtcMarketplace.abi';
 import { ERC20Abi } from '../contracts/abi/ERC20.abi';
 import { Erc20MarketplaceAbi } from '../contracts/abi/Marketplace.abi';
-// import { FaucetAbi } from '../contracts/abi/Faucet.abi';
+import { FaucetAbi } from '../contracts/abi/Faucet.abi';
 import { HexString } from '../types';
 import { Erc20Currencies, Erc20CurrencyTicker } from './currencies';
 import { OrdMarketplaceAbi } from '../contracts/abi/OrdMarketplace.abi';
@@ -37,10 +37,10 @@ const contracts = {
     address: '0x06Bd6358F90F0c83523A4ab9135a61F41cF77850',
     abi: OrdMarketplaceAbi
   },
-  // [ContractType.FAUCET]: {
-  //   address: '0x7884560F14c62E0a83420F17832988cC1a775df1',
-  //   abi: FaucetAbi
-  // }
+  [ContractType.FAUCET]: {
+    address: '0x7884560F14c62E0a83420F17832988cC1a775df1',
+    abi: FaucetAbi
+  }
 } as const;
 
 export { contracts, ContractType };
