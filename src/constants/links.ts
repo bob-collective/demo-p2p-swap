3b05a460542fd2625fa5eedb76da06bcece8486f
Brendon Votteler <bvotteler@users.noreply.github.com>
2024-02-19 14:18:49 +1100

    update bridge url

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,6 @@
 const DOCS_URL = 'https://docs.gobob.xyz/docs/build/examples/btc-swap/';
 const GITHUB_URL = 'https://github.com/bob-collective';
 const FAUCET_URL = 'https://faucetlink.to/sepolia';
-const BRIDGE_URL = 'https://bridge.gobob.xyz//';
+const BRIDGE_URL = 'https://stg-op-bridge.alt.technology/deposit?id=20240201';
 
 export { DOCS_URL, GITHUB_URL, FAUCET_URL, BRIDGE_URL };
