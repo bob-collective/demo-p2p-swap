f94fff89bde21246c4b3669a10c59ea0ff50a90c
Brendon Votteler <bvotteler@users.noreply.github.com>
2024-02-19 21:19:24 +1100

    update incorrect chain id

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const L2_RPC_URL = 'https://op-demo.alt.technology';
-const L2_WSS_URL = undefined;
+const L2_WSS_URL = "wss://op-demo.alt.technology/ws";
 const L2_BLOCK_EXPLORER = 'https://op-demo-explorer.alt.technology';
-const L2_CHAIN_ID = 20240201;
+const L2_CHAIN_ID = 20240219;
 const L2_MULTICALL3_ADDRESS = undefined;
 
 export { L2_BLOCK_EXPLORER, L2_RPC_URL, L2_WSS_URL, L2_CHAIN_ID, L2_MULTICALL3_ADDRESS };
