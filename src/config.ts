4bdb988d03a0eee3a758102213878b2c1ec5e08a
Brendon Votteler <bvotteler@users.noreply.github.com>
2024-02-19 16:47:41 +1100

    added caldera L2 testnet details

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-const L2_RPC_URL = 'https://op-demo.alt.technology';
-const L2_WSS_URL = undefined;
-const L2_BLOCK_EXPLORER = 'https://op-demo-explorer.alt.technology';
-const L2_CHAIN_ID = 20240201;
+const L2_RPC_URL = 'https://testing-bob.rpc.caldera.xyz/http';
+const L2_WSS_URL = 'wss://testing-bob.rpc.caldera.xyz/ws';
+const L2_BLOCK_EXPLORER = 'https://testing-bob.explorer.caldera.xyz/';
+const L2_CHAIN_ID = 7897872;
 const L2_MULTICALL3_ADDRESS = undefined;
 
 export { L2_BLOCK_EXPLORER, L2_RPC_URL, L2_WSS_URL, L2_CHAIN_ID, L2_MULTICALL3_ADDRESS };
